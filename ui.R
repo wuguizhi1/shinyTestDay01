04e9381b97efa3ac85fbccfac2fdcd1043d3c747
wuguizhi1 <guizhi.wu@genetalks.com>
2017-06-15 11:26:46 +0800

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+
+  headerPanel("Shiny ggplot2 examples"),
+  
+  sidebarPanel(
+    selectInput("dataset", "Choose a dataset:", 
+                choices = c("point", "smooth", "boxplot","path","line")),
+
+    numericInput("obs", "Number of observations to view:", 10)
+  ),
+
+  mainPanel(
+    verbatimTextOutput("summary"),
+
+    tableOutput("view")
+  )
+))
