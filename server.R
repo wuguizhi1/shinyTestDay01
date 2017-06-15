429b433a3b0849dd74db0c2c4f31b66b522a7440
wuguizhi1 <guizhi.wu@genetalks.com>
2017-06-15 13:28:59 +0800

    Create server.R

--- server.R
+++ server.R
@@ -14,7 +14,7 @@ shinyServer(function(input, output) {
   
   output$summary <- renderPlot({
     dataset <- datasetInput()
-    qplot(x=c(1,2,3,4,5,5,6,7,8),y=c(2,4,7,14,27,34,47,54,67),geom = c(dataset))
+    qplot(x=c(1,2,3,4,5,5,6,7,8),y=c(2,4,7,14,27,34,47,54,67),geom = c("point"))
   })
 
 })
