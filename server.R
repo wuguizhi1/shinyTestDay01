1077656c5af221ceb4714ee66090d13439417ef7
wuguizhi1 <guizhi.wu@genetalks.com>
2017-06-15 11:30:21 +0800

    Create server.R

---
+++ server.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(ggplot2)
+
+shinyServer(function(input, output) {    
+  datasetInput <- reactive({    
+    switch(input$dataset,           
+      "point" = point,           
+      "smooth" = smooth,           
+      "boxplot" = boxplot,           
+      "path" = path,           
+      "line" = line })
+#           "histogram" = histogram,
+#           "freqpoly" = freqpoly,
+#           "density" = density,
+#           "bar" = bar  })
+
+  output$summary <- renderPrint({    
+    type <- datasetInput()    
+    qplot(x=c(1,2,3,4,5,5,6,7,8),y=c(2,4,7,14,27,34,47,54,67),geom = c(type))      
+  })  
+  
+  output$view <- renderTable({    
+    head(datasetInput(), n = input$obs)  
+  })
+})
+  
