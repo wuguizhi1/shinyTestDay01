fea3da9429798c58c84b08a7ff4cb0ae8fc011a3
wuguizhi1 <guizhi.wu@genetalks.com>
2017-06-15 13:02:42 +0800

    Create server.R

--- server.R
+++ server.R
@@ -1,10 +1,8 @@
 library(shiny)
-library(datasets)
+library(ggplot2)
 
-# Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
 
-  # Return the requested dataset
   datasetInput <- reactive({
     switch(input$dataset,
            "point" = point,
@@ -13,16 +11,10 @@ shinyServer(function(input, output) {
            "path" = path,
            "line" = line)
   })
-
-  # Generate a summary of the dataset
   
   output$summary <- renderPlot({
     dataset <- datasetInput()
     qplot(x=c(1,2,3,4,5,5,6,7,8),y=c(2,4,7,14,27,34,47,54,67),geom = c(dataset))
   })
 
-  # Show the first "n" observations
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
-  })
 })
