ac0afbaf4041309c916eab790a3356067e115b49
wuguizhi1 <guizhi.wu@genetalks.com>
2017-06-15 14:05:46 +0800

    Create ui.R

--- ui.R
+++ ui.R
@@ -1,19 +1,26 @@
 library(shiny)
 
+# Define UI for dataset viewer application
 shinyUI(pageWithSidebar(
 
-  headerPanel("Shiny ggplot2 examples"),
-  
+  # Application title
+  headerPanel("Shiny Text"),
+
+  # Sidebar with controls to select a dataset and specify the number
+  # of observations to view
   sidebarPanel(
     selectInput("dataset", "Choose a dataset:", 
-                choices = c("point", "smooth", "boxplot","path","line")),
+                choices = c("rock", "pressure", "cars")),
 
     numericInput("obs", "Number of observations to view:", 10)
   ),
 
+  # Show a summary of the dataset and an HTML table with the requested
+  # number of observations
   mainPanel(
-    plotOutput("summary")#,
+    verbatimTextOutput("summary"),
 
- #   tableOutput("view")
+    tableOutput("view")
   )
 ))
+
