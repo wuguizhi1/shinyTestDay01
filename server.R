53a5187580fce71d8ccb5c35a9a32cdf3b12f320
wuguizhi1 <guizhi.wu@genetalks.com>
2017-06-15 12:56:15 +0800

    Create server.R

--- server.R
+++ server.R
@@ -16,7 +16,7 @@ shinyServer(function(input, output) {
 
   # Generate a summary of the dataset
   
-  output$summary <- renderPrint({
+  output$summary <- renderPlott({
     dataset <- datasetInput()
     qplot(x=c(1,2,3,4,5,5,6,7,8),y=c(2,4,7,14,27,34,47,54,67),geom = c(dataset))
   })
